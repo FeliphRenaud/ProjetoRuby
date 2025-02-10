8666757566a14ff1f964f97f2b4ccc74bfce38fe
Feliph Renaud <feliphrenaud@gmail.com>
2025-02-10 17:27:18 -0300

    commit de atualizacao do projeto criando a funcionalidade de exclusao de cliente

--- turismo_ms/app/controllers/clientes_controller.rb
+++ turismo_ms/app/controllers/clientes_controller.rb
@@ -1,5 +1,8 @@
 class ClientesController < ApplicationController
   layout 'authentication'
+
+  before_action :require_admin, only: %i[index destroy]
+
   def new
     @cliente = Cliente.new
   end
@@ -28,7 +31,43 @@ class ClientesController < ApplicationController
     end
   end
 
+  def client_management
+    # Verifica  usuário logado é admin
+    unless current_cliente && current_cliente.admin?
+      redirect_to root_path, alert: 'Acesso negado.'
+      return
+    end
+
+    # Carrega os administradores (exceto o admin logado) em ordem alfabética
+    @admins = Cliente.where(admin: true)
+                     .where.not(id: current_cliente.id)
+                     .order(:nome)
+    # Carrega os clientes não administradores em ordem alfabética
+    @non_admins = Cliente.where(admin: false)
+                         .order(:nome)
+
+    # Renderiza usando o layout "application" (que possui header, footer, etc.)
+    render layout: 'application'
+  end
+
+  # Ação para exclusão de um cliente
+  def destroy
+    @cliente = Cliente.find(params[:id])
+    if @cliente == current_cliente
+      redirect_to client_management_path, alert: 'Você não pode excluir sua própria conta.'
+    else
+      @cliente.destroy
+      redirect_to client_management_path, notice: 'Cliente excluído com sucesso.'
+    end
+  end
+
   private
+
+  def require_admin
+    return if current_cliente && current_cliente.admin?
+
+    redirect_to root_path, alert: 'Acesso negado. Somente administradores podem acessar esta página.'
+  end
 
   def cliente_params
     params.require(:cliente).permit(:nome, :email, :password, :password_confirmation)
--- turismo_ms/config/routes.rb
+++ turismo_ms/config/routes.rb
@@ -18,11 +18,12 @@ Rails.application.routes.draw do
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post '/login', to: 'sessions#create'
-
   delete '/logout', to: 'sessions#destroy'
 
   resources :posts
-  resources :clientes, only: %i[new create edit update]
-  resources :admins, only: [:new, :create]
+  resources :clientes, only: %i[new create edit update index destroy]
+  resources :admins, only: %i[new create]
 
+  # Rota para o gerenciamento de clientes
+  get 'client_management', to: 'clientes#client_management'
 end
